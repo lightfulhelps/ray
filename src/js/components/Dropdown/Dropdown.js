68cf02ae8bf3978ede90f033272abd4a4c2c650f
brhgreen <benedictgreen@gmail.com>
2018-08-22 17:13:17 +0100

    dropdown working

--- src/js/components/Dropdown/Dropdown.js
+++ src/js/components/Dropdown/Dropdown.js
@@ -1,9 +1,18 @@
 import * as React from 'react';
+import '../../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 
-const Dropdown = () => {
-  return (
-    <div>Dropdown</div>
-  )
-}
 
-export default Dropdown
+const Dropdown = () => (
+  <div class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      Dropdown button
+  </button>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+      <a class="dropdown-item" href="#">Action</a>
+      <a class="dropdown-item" href="#">Another action</a>
+      <a class="dropdown-item" href="#">Something else here</a>
+    </div>
+  </div>
+)
+
+export default Dropdown;
--- src/js/stories/Dropdown.stories.js
+++ src/js/stories/Dropdown.stories.js
@@ -1,29 +1,12 @@
+
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { withKnobs, text, select, boolean } from '@storybook/addon-knobs';
+import { withKnobs } from '@storybook/addon-knobs';
 import { Container, Row, Dropdown } from '../';
 
 import '../../scss/ray.scss';
 
 const stories = storiesOf('Dropdown', module);
-
-const themes = {
-  primary: 'Primary',
-  secondary: 'Secondary',
-  success: 'Success',
-  danger: 'Danger',
-  warning: 'Warning',
-  info: 'Info',
-  light: 'Light',
-  dark: 'Dark',
-};
-
-const sizes = {
-  lg: 'Large',
-  md: 'Medium',
-  sm: 'Small',
-};
 
 stories.addDecorator(withKnobs);
 
