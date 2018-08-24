e285b2ff885308e8e2ca11263864e994a25e1cf0
Joseph Race <josephrace@users.noreply.github.com>
2018-08-24 14:40:50 +0100

    Icons (#9)
    * SVG icons, build script and Icon component

---
+++ src/js/components/Icon/icons.spec.js
@@ -0,0 +1,7 @@
+import icons from './icons';
+
+describe('icons', () => {
+  it('should match the snapshot', () => {
+    expect(icons).toMatchSnapshot();
+  });
+});
--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ export { default as Button } from './components/Button/Button';
 export { default as Card } from './components/Card/Card';
 export { default as Col } from './components/Col/Col';
 export { default as Container } from './components/Container/Container';
+export { default as Icon } from './components/Icon/Icon';
 export { default as PostCard } from './components/PostCard/PostCard';
 export { default as PostContent } from './components/PostContent/PostContent';
 export { default as PostHeader } from './components/PostHeader/PostHeader';
