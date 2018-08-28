c0006264b8e187ad90a3e8a8d1607559aff04da2
BRHGreen <benedictgreen@gmail.com>
2018-08-28 11:50:55 +0100

    icon in button

--- src/js/components/Button/Button.js
+++ src/js/components/Button/Button.js
@@ -1,6 +1,8 @@
 // @flow
 import * as React from 'react';
+import { color, number } from '@storybook/addon-knobs';
 import classNames from 'classnames';
+import Icon from '../Icon/Icon';
 
 type Props = {
   children: React.Element<any>,
@@ -37,6 +39,12 @@ const Button = ({
   return (
     <Tag className={classes} onClick={onClick}>
       {children}
+      <Icon
+        name={"addMedia"}
+        size={number('Size', 24)}
+        color={color('Color', '#adb5bd')}
+        hoverColor={color('Hover Color', '#212529')}
+      />
     </Tag>
   );
 };
