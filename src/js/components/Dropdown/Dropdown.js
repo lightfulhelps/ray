b4c1de1caa01de83d23b8e1c21ba368242fbc1c5
brhgreen <benedictgreen@gmail.com>
2018-08-30 11:47:34 +0100

    dropdown in card

--- src/js/components/Dropdown/Dropdown.js
+++ src/js/components/Dropdown/Dropdown.js
@@ -4,31 +4,35 @@ import classNames from 'classnames';
 import '../../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min';
 import DropdownItem from './DropdownItem';
 import Button from '../Button/Button';
+import type { IconNameType } from '../Icon/icons';
 
 type Props = {
+  buttonTheme: string,
   children: string,
   className?: string,
   footer?: string,
-  items: [{
-    icon?: string,
-    label: string,
-  }],
+  items: [
+    {
+      icon?: IconNameType,
+      label: string,
+    },
+  ],
   theme?: string,
 };
 
-const Dropdown = ({ theme = 'dark', items, className, footer }: Props) => {
+const Dropdown = ({ theme = 'dark', buttonTheme = 'light', items, className, footer }: Props) => {
   const classes = classNames(className, 'dropdown-menu', `dropdown-menu-${theme}`);
   return (
     <div className="dropdown">
-      <Button 
+      <Button
         icon="menu"
-        data-toggle={'dropdown'}
-        aria-haspopup={'true'}
-        aria-expanded={'false'}
-        theme={theme}
+        data-toggle="dropdown"
+        aria-haspopup="true"
+        aria-expanded="false"
+        theme={buttonTheme}
       />
       <div className={classes} aria-labelledby="dropdownMenuButton">
-        {items.map(({ icon, label, isFooter }, i) => (
+        {items.map(({ icon, label }, i) => (
           <DropdownItem label={label} icon={icon} key={i} />
         ))}
         {footer && <DropdownItem label={footer} className="dropdown-item-footer" />}
--- src/js/components/PostCard/PostCard.js
+++ src/js/components/PostCard/PostCard.js
@@ -79,27 +79,29 @@ const PostCard = ({
   return (
     <Card {...other} className={classes}>
       <div className={`${blockClass}__header`}>
-        {post.socialIdentity && (
-          <div style={{ width: '35px', height: '35px', minWidth: '35px', minHeight: '35px' }}>
-            <Avatar url={post.socialIdentity.avatar} provider={post.socialProvider} />
-          </div>
-        )}
-        <div className="mx-1" style={{ height: '68px', minWidth: 0 }}>
-          <h1 className={`${blockClass}__title`}>{post.title}</h1>
-          <div className={`${blockClass}__date`}>
-            {(!post.date || isFuture(post.date)) && (
-              <Icon name="unscheduled" size={20} color="#adb5bd" />
-            )}
-            {post.date ? formatDate(post.date, dateFormat) : 'Unscheduled'}
-          </div>
-          {post.campaign && (
-            <Badge className="campaign-tag" color={post.campaign.color}>
-              {post.campaign.name}
-            </Badge>
+        <div className="d-flex">
+          {post.socialIdentity && (
+            <div style={{ width: '35px', height: '35px', minWidth: '35px', minHeight: '35px' }}>
+              <Avatar url={post.socialIdentity.avatar} provider={post.socialProvider} />
+            </div>
           )}
+          <div className="mx-1" style={{ height: '68px', minWidth: 0 }}>
+            <h1 className={`${blockClass}__title`}>{post.title}</h1>
+            <div className={`${blockClass}__date`}>
+              {(!post.date || isFuture(post.date)) && (
+                <Icon name="unscheduled" size={20} color="#adb5bd" />
+              )}
+              {post.date ? formatDate(post.date, dateFormat) : 'Unscheduled'}
+            </div>
+            {post.campaign && (
+              <Badge className="campaign-tag" color={post.campaign.color}>
+                {post.campaign.name}
+              </Badge>
+            )}
+          </div>
         </div>
-        <Dropdown items={
-          [
+        <Dropdown
+          items={[
             {
               label: 'edit',
               icon: 'edit',
@@ -108,8 +110,7 @@ const PostCard = ({
               label: 'delete',
               icon: 'delete',
             },
-          ]
-        }
+          ]}
         />
       </div>
       <Dotdotdot className={`${blockClass}__content`} clamp={5}>
