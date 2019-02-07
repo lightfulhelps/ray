814c9f506abc12eee7d892e5cc69fd4654b86f06
brhgreen <benedictgreen@gmail.com>
2019-02-07 10:58:24 +0000

    blank slate

--- src/js/components/Stepper/Stepper.js
+++ src/js/components/Stepper/Stepper.js
@@ -1,70 +1,30 @@
 // @flow
 import * as React from 'react';
-import classNames from 'classnames';
-import Icon from '../Icon/Icon';
-import type { IconNameType } from '../Icon/icons';
 
-type Props = {
-  children?: React.Node,
-  className?: string,
-  icon?: IconNameType,
-  iconPosition?: 'left' | 'right',
-  isBlock?: boolean,
-  isDisabled?: boolean,
-  isOutline?: boolean,
-  onClick?: (SyntheticMouseEvent<>) => void,
-  size?: 'lg' | 'md' | 'sm',
-  tag?: string,
-  theme?: string,
-  type?: 'button' | 'submit' | 'reset',
-};
+/*
+- number of steps
+- which step we're on
+- what the steps are called 
+- icons done, active and to do
+- hide or show component
+- horizontal mode (desktop)
+- vertical mode (mobile)
+*/
 
-const Stepper = ({
-  children,
-  className,
-  icon,
-  iconPosition = 'left',
-  isBlock,
-  isDisabled,
-  isOutline,
-  onClick,
-  size,
-  tag: Tag = 'button',
-  theme = 'primary',
-  type = 'button',
-  ...other
-}: Props) => {
-  const classes = classNames(
-    className,
-    'btn',
-    `btn${isOutline ? '-outline' : ''}-${theme}`,
-    size ? `btn-${size}` : false,
-    { 'btn-block': isBlock },
-    { disabled: isDisabled },
-    { 'btn-icon': icon && !children },
-    { [`btn-icon-${iconPosition}`]: icon && children }
-  );
-
-  function handleClick(e: SyntheticMouseEvent<>) {
-    if (isDisabled) {
-      e.preventDefault();
-      return;
-    }
-
-    if (typeof onClick === 'function') onClick(e);
+class Stepper extends React.Component {
+  state = {
+    complete: null,
+    active: null,
+    todo: null,
   }
-
-  return (
-    <Tag
-      {...other}
-      className={classes}
-      onClick={handleClick}
-      type={Tag === 'button' ? type : undefined}
-    >
-      {children}
-      {icon && <Icon name={icon} />}
-    </Tag>
-  );
-};
+  render() {
+    return (
+      <div>
+        Stepper
+        <hr />
+      </div>
+    );
+  }
+}
 
 export default Stepper;
--- src/js/stories/Stepper.stories.js
+++ src/js/stories/Stepper.stories.js
@@ -1,60 +1,17 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { withKnobs, text, select, boolean } from '@storybook/addon-knobs';
-import { Container, Row, Col, Stepper } from '../';
-import allIcons from '../components/Icon/icons';
-import themes from './utils/themes';
-import sizes from './utils/sizes';
+import { withKnobs } from '@storybook/addon-knobs';
+import { Container, Stepper } from '../';
 
 import '../../scss/ray.scss';
 
 const stories = storiesOf('Stepper', module);
-
-const icons = {};
-
-Object.keys(allIcons).forEach(key => {
-  icons[key] = key;
-});
-
-const iconPositions = {
-  left: 'left',
-  right: 'right',
-};
 
 stories.addDecorator(withKnobs);
 
 stories.add('Default', () => (
   <Container>
     <h1 className="my-4">Stepper.</h1>
-    <Row>
-      <Col xs={6}>
-        <h2 className="h4 mb-2">Regular.</h2>
-        <Stepper
-          theme={select('Theme', themes, 'primary')}
-          isBlock={boolean('Block', false)}
-          isDisabled={boolean('Disabled', false)}
-          isOutline={boolean('Outline', false)}
-          onClick={action('clicked')}
-          size={select('Size', sizes, 'md')}
-          icon={boolean('Show Icon', false) && select('Icon', icons, 'media')}
-          iconPosition={
-            boolean('Show Icon', false) && select('Icon Position', iconPositions, 'left')
-          }
-        >
-          {text('Text', 'Click Me')}
-        </Stepper>
-      </Col>
-      <Col xs={6}>
-        <h2 className="h4 mb-2">Icon Only.</h2>
-        <Stepper
-          theme={select('Theme', themes, 'primary')}
-          icon={select('Icon', icons, 'media')}
-          isDisabled={boolean('Disabled', false)}
-          isOutline={boolean('Outline', false)}
-          size={select('Size', sizes, 'md')}
-        />
-      </Col>
-    </Row>
+    <Stepper />
   </Container>
 ));
