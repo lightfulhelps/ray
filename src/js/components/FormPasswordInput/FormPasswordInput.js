1d15da91caa78f645a4bc6752b7523b0c94f1aa2
Alex <alex@alexandrews.info>
2019-02-12 15:38:15 +0000

    FormPasswordInput Component (#76)
    * Add FormPasswordInput
    
    * Update snapshot
    
    * Update snapshot for icons to include new icon
    
    * Pass onto inner <input>
    
    * removed type and name from props
    
    * Remove helper class that is unneeded when we have border-0
    
    * Use SCSS variable instead of setting size with pixels
    
    * Fix tests
    
    * Improve testing and component
    
    * Update Preview Icon
    
    * Update Icons snapshot test
    
    * Add name as a pass through
    
    * Replace Preview icon with better export

---
+++ src/js/components/FormPasswordInput/FormPasswordInput.js
@@ -0,0 +1,69 @@
+// @flow
+import * as React from 'react';
+import classNames from 'classnames';
+import Button from '../Button/Button';
+
+type Props = {
+  className?: string,
+  name?: string,
+  onBlur: (SyntheticKeyboardEvent<HTMLInputElement>) => void,
+  onChange: (SyntheticKeyboardEvent<HTMLInputElement>) => void,
+  placeholder?: string,
+  value: string,
+};
+
+type State = {
+  hidePassword: boolean,
+};
+
+class FormPasswordInput extends React.Component<Props, State> {
+  state = {
+    hidePassword: true,
+  };
+
+  static defaultProps = {
+    name: 'password',
+  };
+
+  togglePassword = () => {
+    this.setState(prevState => ({
+      hidePassword: !prevState.hidePassword,
+    }));
+  };
+
+  render() {
+    const { className, onChange, onBlur, value, name, placeholder, ...other } = this.props;
+    const { hidePassword } = this.state;
+    const classes = classNames(className, 'form-password-input', 'input-group');
+
+    return (
+      <div {...other} className={classes}>
+        <input
+          className="form-control"
+          data-test-id="password-input"
+          name={name}
+          onBlur={onBlur}
+          onChange={onChange}
+          placeholder={placeholder}
+          type={hidePassword ? 'password' : 'text'}
+          value={value}
+        />
+        <div className="input-group-append">
+          <Button
+            data-test-id="password-input-button"
+            icon={hidePassword ? 'preview' : 'previewHide'}
+            isOutline
+            onClick={this.togglePassword}
+            theme="gray-600"
+          >
+            <span className="form-password-input__toggle-password-text">
+              {hidePassword ? 'Show' : 'Hide'}
+            </span>
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default FormPasswordInput;
---
+++ src/js/components/FormPasswordInput/FormPasswordInput.spec.js
@@ -0,0 +1,178 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import merge from 'lodash/merge';
+import FormPasswordInput from './FormPasswordInput';
+import Button from '../Button/Button';
+
+const setup = (overrides = {}) => {
+  const props = merge(
+    {
+      onBlur: jest.fn(),
+      onChange: jest.fn(),
+      value: 'Some value',
+    },
+    overrides
+  );
+  const wrapper = shallow(<FormPasswordInput {...props} />);
+
+  return { wrapper, props, button: wrapper.find(Button), input: wrapper.find('input') };
+};
+
+describe('<FormPasswordInput />', () => {
+  it('should render', () => {
+    const { wrapper } = setup();
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should handle className', () => {
+    const { wrapper } = setup();
+
+    wrapper.setProps({ className: 'custom' });
+
+    expect(wrapper.hasClass('form-password-input')).toBe(true);
+    expect(wrapper.hasClass('custom')).toBe(true);
+  });
+
+  it('should pass through other props', () => {
+    const { wrapper } = setup({ tabIndex: 1, id: 'test', className: 'custom' });
+
+    expect(wrapper.prop('tabIndex')).toEqual(1);
+    expect(wrapper.prop('id')).toEqual('test');
+  });
+
+  it('defaults name parameter to password', () => {
+    const { input } = setup();
+
+    expect(input.prop('name')).toEqual('password');
+  });
+
+  describe('input pass throughs', () => {
+    it('passes through placeholder', () => {
+      const placeholder = 'Some placeholder';
+
+      const { input } = setup({ placeholder });
+
+      expect(input.prop('placeholder')).toEqual(placeholder);
+    });
+
+    it('passes through value', () => {
+      const value = 'Some value';
+
+      const { input } = setup({ value });
+
+      expect(input.prop('value')).toEqual(value);
+    });
+
+    it('passes through onChange', () => {
+      const {
+        input,
+        props: { onChange },
+      } = setup();
+
+      input.simulate('change');
+
+      expect(onChange).toHaveBeenCalledTimes(1);
+    });
+
+    it('passes through onBlur', () => {
+      const {
+        input,
+        props: { onBlur },
+      } = setup();
+
+      input.simulate('blur');
+
+      expect(onBlur).toHaveBeenCalledTimes(1);
+    });
+
+    it('passes through name', () => {
+      const name = 'secretPassword';
+      const { input } = setup({ name });
+
+      expect(input.prop('name')).toEqual(name);
+    });
+  });
+
+  describe('end to end test helpers', () => {
+    it('has a data-test-id for the input', () => {
+      const { input } = setup();
+
+      expect(input.prop('data-test-id')).toEqual('password-input');
+    });
+
+    it('has a data-test-id for the button', () => {
+      const { button } = setup();
+
+      expect(button.prop('data-test-id')).toEqual('password-input-button');
+    });
+  });
+
+  describe('show password toggle', () => {
+    it('hides password by default', () => {
+      const { input } = setup();
+
+      expect(input.prop('type')).toEqual('password');
+    });
+
+    it('shows password when state is set', () => {
+      const { wrapper } = setup();
+
+      wrapper.setState({ hidePassword: false });
+
+      const input = wrapper.find('input');
+
+      expect(input.prop('type')).toEqual('text');
+    });
+
+    describe('icon toggle', () => {
+      it('has button for toggle', () => {
+        const { button } = setup();
+
+        expect(button).toHaveLength(1);
+      });
+
+      it('has a button that changes state when clicked', () => {
+        const { button, wrapper } = setup();
+
+        button.simulate('click');
+
+        expect(wrapper.state('hidePassword')).toEqual(false);
+      });
+
+      it('has a button that says Show when password is hidden', () => {
+        const { wrapper } = setup();
+
+        const span = wrapper.find('.form-password-input__toggle-password-text');
+
+        expect(span.text()).toEqual('Show');
+      });
+
+      it('has a button that says Hide when password is hidden', () => {
+        const { wrapper } = setup();
+
+        wrapper.setState({ hidePassword: false });
+
+        const span = wrapper.find('.form-password-input__toggle-password-text');
+
+        expect(span.text()).toEqual('Hide');
+      });
+
+      it('has a preview icon when password is hidden', () => {
+        const { button, wrapper } = setup();
+
+        expect(wrapper.state('hidePassword')).toEqual(true);
+
+        expect(button.prop('icon')).toEqual('preview');
+      });
+
+      it('has a preview-hide icon when password is hidden', () => {
+        const { wrapper } = setup();
+
+        wrapper.setState({ hidePassword: false });
+
+        expect(wrapper.find(Button).prop('icon')).toEqual('previewHide');
+      });
+    });
+  });
+});
