404f8d6754bfc969e381f9db4dbc4386dcdd3d0f
BRHGreen <benedictgreen@gmail.com>
2019-07-14 11:27:04 +0100

    some tests

--- src/js/components/ToggleSwitch/ToggleSwitch.spec.js
+++ src/js/components/ToggleSwitch/ToggleSwitch.spec.js
@@ -19,4 +19,22 @@ describe('<ToggleSwitch />', () => {
 
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should render label', () => {
+    const { wrapper } = setup({ label: 'this is a label' });
+    expect(wrapper).toMatchSnapshot();
+  });
+  it('should reder label on the left is labelAlign prop is set to left', () => {
+    const { wrapper } = setup({ label: 'this is a label', labelAlign: 'left' });
+    expect(wrapper).toMatchSnapshot();
+  });
+  it('should set disabled attribute to true if isDisabled prop is specified', () => {
+    const { wrapper } = setup({ isDisabled: true });
+    expect(wrapper).toMatchSnapshot();
+  });
+  it('should handle onClick events', () => {
+    const { wrapper, props } = setup({ onClick: jest.fn() });
+    wrapper.find('label').simulate('click');
+    expect(props.onClick).toHaveBeenCalled();
+  });
 });
