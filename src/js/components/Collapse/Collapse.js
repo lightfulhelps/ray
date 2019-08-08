4a582f6c5ef69d293368ad5ea4a736cf0bd0ed2b
Joseph Race <jarace87@gmail.com>
2019-08-08 14:34:39 +0100

    Update collapse label size

--- src/js/components/Collapse/Collapse.js
+++ src/js/components/Collapse/Collapse.js
@@ -29,11 +29,12 @@ class Collapse extends React.Component<Props, State> {
     return (
       <div {...other} className={classes}>
         <div
-          className="h5 cursor-pointer"
+          className="h6 cursor-pointer"
           data-test-id="collapse-toggle"
           onClick={this.handleToggle}
         >
-          {label} <Icon theme="gray-600" name={this.state.isOpen ? 'chevronUp' : 'chevronDown'} />
+          {label}{' '}
+          <Icon isActive theme="gray-600" name={this.state.isOpen ? 'chevronUp' : 'chevronDown'} />
         </div>
         {this.state.isOpen && <div data-test-id="collapse-children">{children}</div>}
       </div>
