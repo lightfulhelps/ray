067d95cdb75277197156caa7c0d4132436ed451f
brhgreen <benedictgreen@gmail.com>
2019-09-30 17:52:34 +0100

    testing some more filestack props

--- src/js/components/FileStack/FileStack.js
+++ src/js/components/FileStack/FileStack.js
@@ -11,15 +11,21 @@ const YOUR_API_KEY = process.env.FILESTACK_API_KEY;
 const client = filestack.init(YOUR_API_KEY);
 const options = {
   fromSources: ['local_file_system', 'googledrive', 'facebook'],
-  // displayMode: 'inline',
-  displayMode: 'dropPane',
+  displayMode: 'inline',
+  // displayMode: 'dropPane',
   container: 'filestackContainer',
+  dropPane: {
+    cropFiles: true,
+    customText: 'this is custommmmm',
+    overlay: true,
+    showIcon: false,
+  },
 };
 
 client.picker(options).open();
 
 const FileStack = () => {
-  return <div id="filestackContainer" />;
+  return <div id="filestackContainer" style={{ height: '200px', width: '200px' }} />;
 };
 
 export default FileStack;
