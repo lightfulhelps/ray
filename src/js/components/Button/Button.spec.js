2643d01286cb9d5212cb290cf0b921b11019c6a3
ThomasPalamara <palamara.thomas@gmail.com>
2019-10-04 12:12:05 +0100

    Changing props from color to theme

--- src/js/components/Button/Button.spec.js
+++ src/js/components/Button/Button.spec.js
@@ -38,8 +38,8 @@ describe('<Button />', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should handle the iconColor props', () => {
-    const { wrapper } = setup({ icon: 'create', iconColor: '#ffffff' });
+  it('should handle the iconTheme props', () => {
+    const { wrapper } = setup({ icon: 'create', iconTheme: 'primary' });
 
     expect(wrapper).toMatchSnapshot();
   });
