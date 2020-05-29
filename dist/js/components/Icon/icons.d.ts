1e9a65e171529e33c1d7ea66bc6c772a04ad5c27
Joseph Race <jarace87@gmail.com>
2020-05-29 10:03:11 +0100

    Add badge icon

--- dist/js/components/Icon/icons.d.ts
+++ dist/js/components/Icon/icons.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="react" />
-export declare type IconNameType = 'add' | 'alert' | 'analytics' | 'approvalTick' | 'arrowGrowth' | 'awareness' | 'billing' | 'bridgehub' | 'burgerMenu' | 'calendar' | 'card' | 'caretDown' | 'caretLeft' | 'caretRight' | 'caretUp' | 'characterCount' | 'chat' | 'chevronDown' | 'chevronLeft' | 'chevronRight' | 'chevronUp' | 'click' | 'close' | 'comment' | 'connection' | 'copy' | 'create' | 'dashboard' | 'delete' | 'download' | 'edit' | 'emoji' | 'emojiOutline' | 'facebook' | 'filter' | 'follow' | 'fundraising' | 'grid' | 'hashtag' | 'inspiration' | 'instagram' | 'learning' | 'lightBulb' | 'lightful' | 'lightning' | 'like' | 'link' | 'linkedin' | 'list' | 'lockClosed' | 'lockOpen' | 'mediaLibrary' | 'media' | 'menu' | 'notifications' | 'organisationAccount' | 'pause' | 'plan' | 'play' | 'pointRight' | 'posts' | 'previewHide' | 'preview' | 'publishedPost' | 'questionCircle' | 'question' | 'queuedPost' | 'reply' | 'repost' | 'schedule' | 'scheduledPost' | 'search' | 'settings' | 'share' | 'signOut' | 'socialProfile' | 'spikyTick' | 'star' | 'storyBuilder' | 'tag' | 'tick' | 'tutorial' | 'twitter' | 'user' | 'users';
+export declare type IconNameType = 'add' | 'alert' | 'analytics' | 'approvalTick' | 'arrowGrowth' | 'awareness' | 'badge' | 'billing' | 'bridgehub' | 'burgerMenu' | 'calendar' | 'card' | 'caretDown' | 'caretLeft' | 'caretRight' | 'caretUp' | 'characterCount' | 'chat' | 'chevronDown' | 'chevronLeft' | 'chevronRight' | 'chevronUp' | 'click' | 'close' | 'comment' | 'connection' | 'copy' | 'create' | 'dashboard' | 'delete' | 'download' | 'edit' | 'emoji' | 'emojiOutline' | 'facebook' | 'filter' | 'follow' | 'fundraising' | 'grid' | 'hashtag' | 'inspiration' | 'instagram' | 'learning' | 'lightBulb' | 'lightful' | 'lightning' | 'like' | 'link' | 'linkedin' | 'list' | 'lockClosed' | 'lockOpen' | 'mediaLibrary' | 'media' | 'menu' | 'notifications' | 'organisationAccount' | 'pause' | 'plan' | 'play' | 'pointRight' | 'posts' | 'previewHide' | 'preview' | 'publishedPost' | 'questionCircle' | 'question' | 'queuedPost' | 'reply' | 'repost' | 'schedule' | 'scheduledPost' | 'search' | 'settings' | 'share' | 'signOut' | 'socialProfile' | 'spikyTick' | 'star' | 'storyBuilder' | 'tag' | 'tick' | 'tutorial' | 'twitter' | 'user' | 'users';
 declare const _default: {
     add: JSX.Element;
     alert: JSX.Element;
@@ -7,6 +7,7 @@ declare const _default: {
     approvalTick: JSX.Element;
     arrowGrowth: JSX.Element;
     awareness: JSX.Element;
+    badge: JSX.Element;
     billing: JSX.Element;
     bridgehub: JSX.Element;
     burgerMenu: JSX.Element;
