5470fce1fec8b90f3daeb37f45c7e43bf6b4c747
Joseph Race <jarace87@gmail.com>
2020-09-11 23:02:02 +0100

    Only search for tests in src

--- jest.config.js
+++ jest.config.js
@@ -102,9 +102,7 @@ module.exports = {
   // rootDir: null,
 
   // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //   "<rootDir>"
-  // ],
+  roots: ['<rootDir>/src'],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
