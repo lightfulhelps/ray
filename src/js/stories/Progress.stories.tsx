fa701715f25bca8e6c75626eb733618ef108c4bf
Benedict Green <benedictgreen@gmail.com>
2021-05-20 16:40:42 +0100

    Select size prop

--- src/js/stories/Progress.stories.tsx
+++ src/js/stories/Progress.stories.tsx
@@ -9,31 +9,39 @@ const stories = storiesOf('Progress', module);
 
 stories.addDecorator(withKnobs);
 
-const size = select('Size', { Small: 'sm', Medium: 'md', Large: 'lg' }, 'md');
+const options = { Small: 'sm', Medium: 'md', Large: 'lg' };
 
 stories.add('Default', () => (
   <Container>
     <h1 className="my-4">Progress</h1>
     <div className="mb-2">
       <h2 className="h6">Default</h2>
-      <Progress
-        value={number('Value 1', 25)}
-        size={select('Size', { Small: 'sm', Medium: 'md', Large: 'lg' }, 'md')}
-      />
+      <Progress value={number('Value 1', 25)} size={select('Size', options, 'md')} />
     </div>
     <div className="mb-2">
       <h2 className="h6">With Label</h2>
-      <Progress value={33} size={size}>
+      <Progress value={33} size={select('Size', options, 'md')}>
         33%
       </Progress>
     </div>
     <div className="mb-2">
       <h2 className="h6">Themed and striped</h2>
-      <Progress value={number('Value 2', 50)} size={size} theme="warning" isStriped />
+      <Progress
+        value={number('Value 2', 50)}
+        size={select('Size', options, 'md')}
+        theme="warning"
+        isStriped
+      />
     </div>
     <div className="mb-2">
       <h2 className="h6">Themed, striped and animated</h2>
-      <Progress value={75} size={size} theme="success" isStriped isAnimated />
+      <Progress
+        value={75}
+        size={select('Size', options, 'md')}
+        theme="success"
+        isStriped
+        isAnimated
+      />
     </div>
   </Container>
 ));
