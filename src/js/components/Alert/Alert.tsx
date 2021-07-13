8ce0dccfe00310b8385ba54f6b7bfb881511b592
ThomasPalamara <palamara.thomas@gmail.com>
2021-07-13 13:42:21 +0100

    rework alerts

--- src/js/components/Alert/Alert.tsx
+++ src/js/components/Alert/Alert.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import classNames from 'classnames';
+import Icon from '../Icon/Icon';
+import { IconNameType } from '../Icon/icons';
 
 type Props = {
   className?: string;
+  header?: string;
+  icon?: IconNameType;
   tag?: keyof JSX.IntrinsicElements | typeof React.Component;
   theme?: string;
+  hideIcon?: boolean;
   children: React.ReactNode;
   [key: string]: any;
 };
@@ -12,15 +17,35 @@ type Props = {
 const Alert: React.FC<Props> = ({
   children,
   className,
+  header,
+  icon,
   tag: Tag = 'div',
   theme = 'primary',
+  hideIcon = false,
   ...other
 }: Props) => {
-  const classes = classNames(className, 'alert', `alert-${theme}`);
+  const classes = classNames(className, 'alert ', `alert-${theme}`);
+  const iconsByTheme = {
+    danger: 'alert',
+    success: 'approvalTick',
+    info: 'info',
+  };
 
   return (
     <Tag {...other} className={classes}>
-      {children}
+      {header && <h4 className="alert-heading">{header}</h4>}
+      <div className="d-flex align-items-start">
+        {!hideIcon && (iconsByTheme[theme] || icon) && (
+          <Icon
+            className="mr-half flex-shrink-0"
+            name={icon || iconsByTheme[theme]}
+            theme={theme}
+            size={18}
+            style={{ marginTop: '3px' }}
+          />
+        )}
+        <span>{children}</span>
+      </div>
     </Tag>
   );
 };
