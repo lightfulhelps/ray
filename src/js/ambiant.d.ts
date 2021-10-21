511f4fcc89436d4ffa151e7ed8ad483a8f24eb84
ThomasPalamara <palamara.thomas@gmail.com>
2021-10-21 18:24:11 +0100

    remove stuff

--- src/js/ambiant.d.ts
+++ src/js/ambiant.d.ts
@@ -1,7 +1,2 @@
 declare module 'react-lines-ellipsis/lib/html';
 declare module 'react-lines-ellipsis/lib/responsiveHOC';
-
-declare module '*.png' {
-  const content: any;
-  export default content;
-}
