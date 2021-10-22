eee2a7d4ff863851c3c4312027e3537f6d8bfe1c
ThomasPalamara <palamara.thomas@gmail.com>
2021-10-22 10:24:48 +0100

    PLATFORM-2257: Ray 2.0 | Avatars (#239)
    * new avatars
    
    * remove stuff
    
    * add new bg
    
    * remove unused css

--- src/js/components/Avatar/Avatar.spec.tsx
+++ src/js/components/Avatar/Avatar.spec.tsx
@@ -82,7 +82,7 @@ describe('<Avatar />', () => {
     const { wrapper } = setup({ url: null });
 
     expect(wrapper.find('.avatar-image').prop('style').backgroundImage).toContain(
-      'data:image/svg+xml;utf8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20xmlns%3Axlink%3D%22http%3A%2F%2Fwww.w3.org%2F1999%2Fxlink%22%20style%3D%22isolation%3Aisolate%3B%22%20viewBox%3D%220%200%201%201%22%20version%3D%221.1%22%3E%3Crect%20width%3D%221%22%20height%3D%221%22%20fill%3D%22%236D4C41%22%2F%3E%3Ctext%20x%3D%2250%25%22%20y%3D%2250%25%22%20style%3D%22%20font-family%3A%20Arial%2Csans-serif%3B%20font-size%3A%200.5px%22%20fill%3D%22%23FFF%22%20text-anchor%3D%22middle%22%20dy%3D%22.178%22%3EMF%3C%2Ftext%3E%3C%2Fsvg%3E'
+      'url(data:image/svg+xml;utf8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20xmlns%3Axlink%3D%22http%3A%2F%2Fwww.w3.org%2F1999%2Fxlink%22%20style%3D%22isolation%3Aisolate%3B%22%20viewBox%3D%220%200%201%201%22%20version%3D%221.1%22%3E%3Crect%20width%3D%221%22%20height%3D%221%22%20fill%3D%22transparent%22%2F%3E%3Ctext%20x%3D%2250%25%22%20y%3D%2250%25%22%20style%3D%22%20font-family%3A%20Arial%2Csans-serif%3B%20font-size%3A%200.5px%22%20fill%3D%22%23FFF%22%20text-anchor%3D%22middle%22%20dy%3D%22.178%22%3EMF%3C%2Ftext%3E%3C%2Fsvg%3E), url(https://assets.lightful.com/platform/brand/avatar_background.png)'
     );
   });
 });
