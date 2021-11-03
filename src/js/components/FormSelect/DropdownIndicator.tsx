29a64d08df7681fa273d134f3240616eba847397
ThomasPalamara <palamara.thomas@gmail.com>
2021-11-03 16:06:31 +0000

    chevron changes

--- src/js/components/FormSelect/DropdownIndicator.tsx
+++ src/js/components/FormSelect/DropdownIndicator.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Icon } from '../../';
 
-const DropdownIndicator: React.FC = () => <Icon name="caretDown" title="Open" theme="gray-500" />;
+const DropdownIndicator: React.FC = () => <Icon name="chevronDown" title="Open" theme="gray-500" />;
 
 export default DropdownIndicator;
