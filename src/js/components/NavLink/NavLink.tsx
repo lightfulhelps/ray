6718bb184617a7bec92690396c8aa97635f0f28b
ThomasPalamara <palamara.thomas@gmail.com>
2021-11-19 13:55:17 +0000

    optional children

--- src/js/components/NavLink/NavLink.tsx
+++ src/js/components/NavLink/NavLink.tsx
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 
 type Props = {
   className?: string;
-  children: React.ReactNode;
+  children?: React.ReactNode;
   isActive?: boolean;
   isDisabled?: boolean;
   tag?: keyof JSX.IntrinsicElements | typeof React.Component;
