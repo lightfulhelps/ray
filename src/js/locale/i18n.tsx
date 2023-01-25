b8d7423b79330ca23be1718ecc62db95dc78c32e
Jodie Poole <jodie.poole@lightful.com>
2023-01-25 16:03:15 +0000

    remove test json

--- src/js/locale/i18n.tsx
+++ src/js/locale/i18n.tsx
@@ -2,7 +2,6 @@ import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 import en from './en.json';
-import de from './de.json';
 
 const translation = i18n.createInstance();
 
