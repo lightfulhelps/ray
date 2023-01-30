538d1fa2c20964035ba432406d59d36ab5e271cb
Jodie Poole <jodie.poole@lightful.com>
2023-01-30 08:56:26 +0000

    add language detection using local storage

--- src/js/locale/i18n.tsx
+++ src/js/locale/i18n.tsx
@@ -1,31 +1,15 @@
 import i18n from 'i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 import en from './en.json';
 
 const translation = i18n.createInstance();
 
-translation
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    debug: true,
-    fallbackLng: 'en',
-    supportedLngs: ['en'],
-    resources: { en: { ...en } },
-    detection: {
-      order: [
-        'querystring',
-        'cookie',
-        'sessionStorage',
-        'navigator',
-        'htmlTag',
-        'path',
-        'subdomain',
-        'localStorage',
-      ],
-      lookupQuerystring: 'lng',
-    },
-  });
+translation.use(initReactI18next).init({
+  debug: true,
+  fallbackLng: 'en',
+  lng: localStorage.getItem('userLanguage') ?? 'en',
+  supportedLngs: ['en'],
+  resources: { en: { ...en } },
+});
 
 export default translation;
