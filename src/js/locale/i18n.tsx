0e35f82594801dfedc729f46e324b79970768d44
Michael Hughes <mhughes36@live.co.uk>
2023-02-16 11:49:17 +0000

    Add uk to i18n file

--- src/js/locale/i18n.tsx
+++ src/js/locale/i18n.tsx
@@ -1,6 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import en from './en.json';
+import uk from './uk.json';
 
 const translation = i18n.createInstance();
 
@@ -11,8 +12,11 @@ translation.use(initReactI18next).init({
     escapeValue: false, // not needed for react as it escapes by default
   },
   lng: localStorage.getItem('userLanguage') || 'en',
-  supportedLngs: ['en'],
-  resources: { en: { ...en } },
+  supportedLngs: ['en', 'uk'],
+  resources: {
+    en: { ...en },
+    uk: { ...uk },
+  },
 });
 
 export default translation;
